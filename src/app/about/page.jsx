52d69bd2ef37238c1d038d600aa32410534fcd34
Hridoykhan4 <hridoykhan148385@gmail.com>
2026-06-11 01:16:10 +0600

    Refactor About and Contact pages: add navigation logic and enhance layout with links and buttons

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,29 @@
-const page = () => {
+// Eta use korte hobe shobar upore
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+const AboutPage = () => {
+    // Carefully import koris
+    const router = useRouter()
+    const isLoggedIn = true
+
+    const handleNavigation = () => {
+        if (isLoggedIn) {
+            router.push('/about/address')
+        }
+        else {
+            router.push('/')
+        }
+    }
+
     return (
         <div>
             <p className="text-3xl font-bold">About Page</p>
+            <Link href="/about/address" className="underline">Visit address</Link>
+            <button className="block underline decoration-sky-500" type="button" onClick={handleNavigation}>Address Page</button>
         </div>
     );
 };
 
-export default page;
+export default AboutPage;
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,24 @@
+'use client'
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 const ContactPage = () => {
+    const router = useRouter();
+    const isLoggedIn = false
+    const handleOffice = () => {
+        if (isLoggedIn) {
+            router.push('/contact/offices')
+        } else {
+            router.push('/')
+        }
+    }
+
     return (
         <div>
-            Contact Page
+            <p className="text-3xl font-bold">Contact Page</p>
+            <Link href="/contact/offices" className="underline">Visit Offices</Link>
+            {/* Type submit dis */}
+            <button type="button" className="block underline decoration-red-600" onClick={handleOffice}>Visit Offices</button>
         </div>
     );
 };
