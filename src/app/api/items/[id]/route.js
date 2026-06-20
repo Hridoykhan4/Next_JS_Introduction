8834924bbce9968c967dd4dbd4515aeca63fb2b8
Hridoykhan4 <hridoykhan148385@gmail.com>
2026-06-20 22:56:38 +0600

    Refactor API route handlers for items; streamline parameter extraction and enhance DELETE method implementation.

--- src/app/api/items/[id]/route.js
+++ src/app/api/items/[id]/route.js
@@ -1,16 +1,28 @@
 import dbConnect from "@/lib/dbConnect";
 import { ObjectId } from "mongodb";
 
-export async function GET(req, {params}) {
-  const p = await params;
-  console.log(p.id);
+export async function GET(req, { params }) {
+  const { id } = await params;
   const result = await dbConnect("practice_data").findOne({
-    _id: new ObjectId(p.id),
+    _id: new ObjectId(id),
   });
   return Response.json(result);
 }
 
+export async function DELETE(req, { params }) {
+  const { id } = await params;
+  const result = await dbConnect("practice_data").deleteOne({
+    _id: new ObjectId(id),
+  });
+  return Response.json(result);
+}
 
-// export async  function DELETE(req, params) {
-
-// }    
+export async function PATCH(req, { params }) {
+  const test = await req.json();
+  const { id } = await params;
+  const result = await dbConnect("practice_data").updateOne(
+    { _id: new ObjectId(id) },
+    { $set: { ...test } },
+  );
+  return Response.json(result);
+}
